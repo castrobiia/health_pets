e421c15baec59c45a550156910d468541ceb40ce
gabipz <gabriela.pss@hotmail.com>
2022-02-02 21:49:13 -0300

    Adiçao da PetPage

---
+++ lib/pages/pet.page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:health_pets/pages/menu.page.dart';
+
+class PetPage extends StatelessWidget {
+  const PetPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pets"),
+        actions: [
+          PopupMenuButton<MenuItem>(
+            onSelected: (item) => onSelected(context, item),
+            itemBuilder: (context) => [
+              ...MenuItems.items.map(buildItem).toList(),
+            ],
+          ),
+        ],
+      ),
+      body: Container(
+        color: Colors.red[100],
+        child: Center(
+          child: Text("Teste"),
+        ),
+      ),
+    );
+  }
+}
