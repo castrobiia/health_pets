2db26015f5ccd58cc7a152494dcbf275b6250b72
castrobiia <beatriz.castro@creditas.com>
2022-02-04 22:52:48 -0300

    alteracao do card de pets

--- lib/widges/pet/pet-card.widget.dart
+++ lib/widges/pet/pet-card.widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class PetCard extends StatelessWidget {
-
   final String image;
   final String petName;
   final String taxonomy;
@@ -17,34 +16,53 @@ class PetCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 120,
-      margin: EdgeInsets.all(5),
-    child: Row(
-      children: <Widget>[
-        Container(
-          width: 100,
-          height: 100,
-          margin: EdgeInsets.all(10),
-          child: Image.asset(
-            image,
-            fit: BoxFit.fitWidth,
+      height: 100,
+      //margin: EdgeInsets.all(5),
+      child: Row(
+        //mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.only(left: 10),
+            child: Image.asset(
+              image,
+              fit: BoxFit.fitWidth,
+            ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(
-            top: 20,
-            left: 10,
+          Padding(
+            padding: EdgeInsets.only(top: 20, left: 10, right: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  petName,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(taxonomy),
+                //Text("${age} anos"), // fazer um if/switch pra: ano, mes, meses
+              ],
+            ),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(petName),
-              Text(taxonomy),
-              Text("${age} anos"), // fazer um if/switch pra: ano, mes, meses
-            ],),
-        ),
-      ],
-    ),
-  );
+          SizedBox(
+            width: 110,
+          ),
+          Container(
+            width: 80,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Icon(Icons.navigate_next)
+                //Icon(Icons.edit),
+                //Icon(Icons.delete),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
