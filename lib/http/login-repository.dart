c963b6532532b1b4f6ab54acf424daa66acd70ca
castrobiia <beatriz.castro@creditas.com>
2022-03-22 21:24:37 -0300

    excluindo código hive

--- lib/http/login-repository.dart
+++ lib/http/login-repository.dart
@@ -1,7 +1,6 @@
 import 'package:health_pets/models/login-model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:typed_data';
-import 'package:hive/hive.dart';
 
 class LoginRepository {
   Future<LoginModel?> login(String email, String password) async {
@@ -16,13 +15,6 @@ class LoginRepository {
       'email': email,
       'password': password,
     });
-
-    var keyBox = await Hive.openBox('session');
-    // if (!keyBox.containsKey('key')) {
-    //   // redirecionar tela de login
-    // } else {
-    //   // mensagem
-    // }
 
     print('RESPONSE STATUS CODE: ${response.statusCode}');
     print('RESPONSE BODY: ${response.body}');
