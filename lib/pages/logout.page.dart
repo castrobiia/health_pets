46261e50cffb3b0962f7030db1bddee738d92a6c
Gustavo Pessoa <gupessoa@live.com>
2022-03-30 22:12:19 -0300

    Crianod a função de logout do sistema

--- lib/pages/logout.page.dart
+++ lib/pages/logout.page.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:health_pets/links/links-pages.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+import 'login.page.dart';
 
 class LogoutPage extends StatefulWidget {
   const LogoutPage({Key? key}) : super(key: key);
@@ -7,9 +14,35 @@ class LogoutPage extends StatefulWidget {
   _LogoutPageState createState() => _LogoutPageState();
 }
 
+Future deleteToken(BuildContext context) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String token = await prefs.get('token').toString();
+
+  var header = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Authorization": "Bearer ${token}"
+  };
+
+  const url = 'https://www.healthpets.app.br/api/auth/logout';
+  final response =
+  await http.get(Uri.parse(url), headers: header);
+
+  if(response.statusCode == 200){
+    prefs.clear();
+    setarMaterialPageRoute(context, LoginPage());
+  }
+
+  return Container(
+    child: Text('Bye Bye'),
+  );
+}
+
 class _LogoutPageState extends State<LogoutPage> {
+
   @override
   Widget build(BuildContext context) {
+    deleteToken(context);
     return Scaffold(
       body: Center(
         child: Column(
@@ -21,10 +54,29 @@ class _LogoutPageState extends State<LogoutPage> {
             SizedBox(height: 10),
             Container(
               child: CircularProgressIndicator(),
-            )
+            ),
           ],
         ),
       ),
     );
   }
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     body: Center(
+  //       child: Column(
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         children: <Widget>[
+  //           Text(
+  //             "Saindo...",
+  //           ),
+  //           SizedBox(height: 10),
+  //           Container(
+  //             child: CircularProgressIndicator(),
+  //           )
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 }
