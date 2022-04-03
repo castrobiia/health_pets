325770ab12854874f8c2969dc0a464af5902f57c
castrobiia <beatriz.castro@creditas.com>
2022-04-03 20:17:00 -0300

    retornar nome da espécie e raça do animal no editar animal

--- lib/pages/editar-pet.page.dart
+++ lib/pages/editar-pet.page.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:health_pets/links/links-pages.dart';
+import 'package:health_pets/pages/perfil-pet.page.dart';
 import 'package:health_pets/pages/pet.page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
@@ -36,6 +37,44 @@ Future<dynamic?> getAnimal(int id) async {
   return animal;
 }
 
+Future<dynamic?> getEspecie(int id) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String token = await prefs.get('token').toString();
+
+  var header = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Authorization": "Bearer ${token}"
+  };
+
+  const url = 'https://www.healthpets.app.br/api/especie/';
+  final response =
+      await http.get(Uri.parse(url + id.toString()), headers: header);
+
+  dynamic especie = jsonDecode(response.body);
+
+  return especie;
+}
+
+Future<dynamic?> getRaca(int id) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String token = await prefs.get('token').toString();
+
+  var header = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Authorization": "Bearer ${token}"
+  };
+
+  const url = 'https://www.healthpets.app.br/api/raca/';
+  final response =
+      await http.get(Uri.parse(url + id.toString()), headers: header);
+
+  dynamic raca = jsonDecode(response.body);
+
+  return raca;
+}
+
 class _EditarPetPageState extends State<EditarPetPage> {
   final id;
   _EditarPetPageState(this.id);
@@ -56,171 +95,216 @@ class _EditarPetPageState extends State<EditarPetPage> {
       ),
       body: SingleChildScrollView(
         child: Container(
-            height: double.maxFinite,
-            color: Colors.white,
-            child: Padding(
-                padding: EdgeInsets.only(
-                  left: 30,
-                  right: 30,
-                  top: 30,
-                ),
-                child: FutureBuilder<dynamic>(
-                    future: getAnimal(id),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState != ConnectionState.done) {
-                        // return: show loading widget
-                        //todo mostrar o loading
-                        return Center(
-                            child:
-                                Container(child: CircularProgressIndicator()));
-                      }
-                      if (snapshot.hasError) {
-                        // return: show error widget
-                        return Center(
-                            child: Container(
-                                child: Text('Erro ao carregar os dados')));
-                        print(snapshot.data);
-                      }
+          height: double.maxFinite,
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: 30,
+              right: 30,
+              top: 30,
+            ),
+            child: FutureBuilder<dynamic>(
+              future: getAnimal(id),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState != ConnectionState.done) {
+                  // return: show loading widget
+                  //todo mostrar o loading
+                  return Center(
+                      child: Container(child: CircularProgressIndicator()));
+                }
+                if (snapshot.hasError) {
+                  // return: show error widget
+                  return Center(
+                      child:
+                          Container(child: Text('Erro ao carregar os dados')));
+                }
 
-                      final animal = snapshot.data;
+                final animal = snapshot.data;
 
-                      return Column(
-                        children: <Widget>[
-                          Stack(
+                return FutureBuilder<dynamic>(
+                  future: getEspecie(animal['id_especie']),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState != ConnectionState.done) {
+                      // return: show loading widget
+                      //todo mostrar o loading
+                      return Center(
+                          child: Container(child: CircularProgressIndicator()));
+                    }
+                    if (snapshot.hasError) {
+                      // return: show error widget
+                      return Center(
+                          child: Container(
+                              child: Text('Erro ao carregar os dados')));
+                    }
+
+                    final especie = snapshot.data;
+
+                    return FutureBuilder<dynamic>(
+                        future: getRaca(animal['id_raca']),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState !=
+                              ConnectionState.done) {
+                            // return: show loading widget
+                            //todo mostrar o loading
+                            return Center(
+                                child: Container(
+                                    child: CircularProgressIndicator()));
+                          }
+                          if (snapshot.hasError) {
+                            // return: show error widget
+                            return Center(
+                                child: Container(
+                                    child: Text('Erro ao carregar os dados')));
+                          }
+
+                          final raca = snapshot.data;
+
+                          return Column(
                             children: <Widget>[
-                              Container(
-                                width: 180,
-                                height: 180,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  image: DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image:
-                                        AssetImage(animal['foto'].toString()),
+                              Stack(
+                                children: <Widget>[
+                                  Container(
+                                    width: 180,
+                                    height: 180,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      image: DecorationImage(
+                                        fit: BoxFit.cover,
+                                        image: AssetImage(
+                                            animal['foto'].toString()),
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    top: 120,
+                                    left: 120,
+                                    child: FloatingActionButton(
+                                      child: Icon(Icons
+                                          .add_a_photo), //ou upload, add, image
+                                      backgroundColor: Color(0xFFF6BD87),
+                                      //cor do ícone
+                                      foregroundColor: Colors.white,
+                                      onPressed: () {},
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              TextFormField(
+                                autofocus: false,
+                                keyboardType: TextInputType.text,
+                                initialValue: animal['nome'],
+                                decoration: InputDecoration(
+                                  labelText: "Nome",
+                                  labelStyle: TextStyle(
+                                    //color: Color(0xFFCC9396),
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 17,
                                   ),
                                 ),
                               ),
-                              Positioned(
-                                top: 120,
-                                left: 120,
-                                child: FloatingActionButton(
-                                  child: Icon(Icons
-                                      .add_a_photo), //ou upload, add, image
-                                  backgroundColor: Color(0xFFF6BD87),
-                                  //cor do ícone
-                                  foregroundColor: Colors.white,
-                                  onPressed: () {},
+                              SizedBox(
+                                height: 10,
+                              ),
+                              TextFormField(
+                                //formatar para receber data
+                                autofocus: false,
+                                initialValue: animal['data_nascimento'],
+                                keyboardType: TextInputType.datetime,
+                                decoration: InputDecoration(
+                                  labelText: "Data de Nascimento",
+                                  labelStyle: TextStyle(
+                                    //color: Color(0xFFCC9396),
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 17,
+                                  ),
                                 ),
                               ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              TextFormField(
+                                autofocus: false,
+                                initialValue: especie['descricao'].toString(),
+                                keyboardType: TextInputType.text,
+                                decoration: InputDecoration(
+                                  labelText: "Espécie",
+                                  labelStyle: TextStyle(
+                                    //color: Color(0xFFCC9396),
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 17,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              TextFormField(
+                                autofocus: false,
+                                keyboardType: TextInputType.text,
+                                initialValue: raca['descricao'].toString(),
+                                decoration: InputDecoration(
+                                  labelText: "Raça",
+                                  labelStyle: TextStyle(
+                                    //color: Color(0xFFCC9396),
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 17,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              SizedBox(
+                                height: 50,
+                              ),
+                              Container(
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  color: Color(0xFFF6BD87),
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(5),
+                                  ),
+                                  boxShadow: [
+                                    new BoxShadow(
+                                      //cor do fundo (em volta) do container
+                                      color: Colors.black12,
+                                      //qnt de sombra
+                                      offset: new Offset(1, 2.0),
+                                      //expansao da sombra
+                                      blurRadius: 5,
+                                      //intensidade da borda
+                                      spreadRadius: 3,
+                                    ),
+                                  ],
+                                ),
+                                child: TextButton(
+                                  onPressed: () {
+                                    setarMaterialPageRoute(context, PetPage());
+                                  },
+                                  child: Text(
+                                    "Salvar",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 17),
+                                  ),
+                                ),
+                              ),
+                              /* DropdownButtonFormField(
+                      value: ValorInicialEspecie,
+                      items: especies.map((items) {
+                        return DropdownMenuItem(value: items, child: Text(items));
+                      }).toList(),
+                      onChanged: (value) {}) */
                             ],
-                          ),
-                          TextFormField(
-                            autofocus: false,
-                            keyboardType: TextInputType.text,
-                            initialValue: animal['nome'],
-                            decoration: InputDecoration(
-                              labelText: "Nome",
-                              labelStyle: TextStyle(
-                                //color: Color(0xFFCC9396),
-                                fontWeight: FontWeight.w400,
-                                fontSize: 17,
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          TextFormField(
-                            autofocus: false,
-                            initialValue: animal['id_especie'].toString(),
-                            keyboardType: TextInputType.text,
-                            decoration: InputDecoration(
-                              labelText: "Espécie",
-                              labelStyle: TextStyle(
-                                //color: Color(0xFFCC9396),
-                                fontWeight: FontWeight.w400,
-                                fontSize: 17,
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          TextFormField(
-                            autofocus: false,
-                            keyboardType: TextInputType.text,
-                            initialValue: animal['id_raca'].toString(),
-                            decoration: InputDecoration(
-                              labelText: "Raça",
-                              labelStyle: TextStyle(
-                                //color: Color(0xFFCC9396),
-                                fontWeight: FontWeight.w400,
-                                fontSize: 17,
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          TextFormField(
-                            //formatar para receber data
-                            autofocus: false,
-                            initialValue: animal['data_nascimento'],
-                            keyboardType: TextInputType.datetime,
-                            decoration: InputDecoration(
-                              labelText: "Data de Nascimento",
-                              labelStyle: TextStyle(
-                                //color: Color(0xFFCC9396),
-                                fontWeight: FontWeight.w400,
-                                fontSize: 17,
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 50,
-                          ),
-                          Container(
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                              color: Color(0xFFF6BD87),
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(5),
-                              ),
-                              boxShadow: [
-                                new BoxShadow(
-                                  //cor do fundo (em volta) do container
-                                  color: Colors.black12,
-                                  //qnt de sombra
-                                  offset: new Offset(1, 2.0),
-                                  //expansao da sombra
-                                  blurRadius: 5,
-                                  //intensidade da borda
-                                  spreadRadius: 3,
-                                ),
-                              ],
-                            ),
-                            child: TextButton(
-                              onPressed: () {
-                                setarMaterialPageRoute(context, PetPage());
-                              },
-                              child: Text(
-                                "Salvar",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w700,
-                                    fontSize: 17),
-                              ),
-                            ),
-                          ),
-                          /* DropdownButtonFormField(
-                        value: ValorInicialEspecie,
-                        items: especies.map((items) {
-                          return DropdownMenuItem(value: items, child: Text(items));
-                        }).toList(),
-                        onChanged: (value) {}) */
-                        ],
-                      );
-                    }))),
+                          );
+                        });
+                  },
+                );
+              },
+            ),
+          ),
+        ),
       ),
     );
   }
