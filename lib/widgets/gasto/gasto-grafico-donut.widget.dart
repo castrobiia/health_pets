a3d598e6aba77ff249bbfe96389091a7bc1d9a5d
gabipz <gabriela.pss@hotmail.com>
2022-04-17 12:20:11 -0300

    Adição do centro do donut

--- lib/widgets/gasto/gasto-grafico-donut.widget.dart
+++ lib/widgets/gasto/gasto-grafico-donut.widget.dart
@@ -32,6 +32,15 @@ class _GraficoDonutWidgetState extends State<GraficoDonutWidget> {
   }
 }
 
+final linePaint = Paint()
+  ..color = Colors.white
+  ..strokeWidth = 2.0
+  ..style = PaintingStyle.stroke;
+
+final midPaint = Paint()
+  ..color = Colors.white24
+  ..style = PaintingStyle.fill;
+
 class DonutChartPainter extends CustomPainter {
   final List<Dados> dataset;
   DonutChartPainter(this.dataset);
@@ -41,17 +50,34 @@ class DonutChartPainter extends CustomPainter {
     final c = Offset(size.width / 2.0, size.height / 2.0);
     final radius = size.width * 0.9;
     final rect = Rect.fromCenter(center: c, width: radius, height: radius);
-
     var startAngle = 0.0;
-    dataset.forEach((element) {
-      final sweepAngle = element.value * 360.0 * pi / 180.0;
-      final paint = Paint()
-        ..style = PaintingStyle.fill
-        ..color = element.color;
-
-      canvas.drawArc(rect, startAngle, sweepAngle, true, paint);
+    
+    dataset.forEach((di) {
+      final sweepAngle = di.value * 360.0 * pi / 180.0;
+      drawSelectors(canvas, di, rect, startAngle, sweepAngle);
       startAngle += sweepAngle;
     });
+    dataset.forEach((di) {
+      final sweepAngle = di.value * 360.0 * pi / 180.0;
+      drawLine(canvas, c, radius, startAngle);
+      startAngle += sweepAngle;
+    });
+    canvas.drawCircle(c, radius * 0.3, midPaint);
+  }
+
+
+  void drawLine(Canvas canvas, Offset c, double radius, double startAngle) {
+    final dx = radius / 2.0 * cos(startAngle);
+    final dy = radius / 2.0 * sin(startAngle);
+    final p2 = c + Offset(dx, dy);
+    canvas.drawLine(c, p2, linePaint);
+  }
+
+  void drawSelectors(Canvas canvas, Dados di, Rect rect, double startAngle, double sweepAngle) {
+    final paint = Paint()
+      ..style = PaintingStyle.fill
+      ..color = di.color;
+    canvas.drawArc(rect, startAngle, sweepAngle, true, paint);
   }
 
   @override
