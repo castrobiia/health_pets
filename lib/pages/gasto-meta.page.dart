672f781ddf58208df3e590ad304f827f7ca05b71
gabipz <gabriela.pss@hotmail.com>
2022-04-19 22:04:43 -0300

    Remoção dos comentários

--- lib/pages/gasto-meta.page.dart
+++ lib/pages/gasto-meta.page.dart
@@ -11,32 +11,6 @@ class _MetaGastoState extends State<MetaGasto> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.spaceAround,
-//           children: <Widget>[
-//             Padding(
-//               padding: EdgeInsets.all(15.0),
-              
-//               child: new LinearPercentIndicator(
-//                 width: MediaQuery.of(context).size.width - 50,
-//                 animation: true,
-//                 lineHeight: 20.0,
-//                 animationDuration: 2500,
-//                 percent: 0.8,
-//                 center: Text("80.0%"),
-//                 linearStrokeCap: LinearStrokeCap.roundAll,
-//                 progressColor: Colors.green,
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
