732531f977eb5b43c60b4c4883866462625cad12
“gabipz” <gabriela.suarez@creditas.com>
2022-05-14 19:15:26 -0300

    Alerta da exclusão da vacina

--- lib/pages/alert-dialog.dart
+++ lib/pages/alert-dialog.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:health_pets/controllers/animal_controller.dart';
 import 'package:health_pets/controllers/usuario_controller.dart';
+import 'package:health_pets/controllers/vacina_controller.dart';
 import 'package:health_pets/pages/login.page.dart';
 import 'package:health_pets/pages/logout.page.dart';
 import 'package:health_pets/pages/tabs-perfil-pet.page.dart';
 import 'package:health_pets/pages/tabs.page.dart';
 import 'package:health_pets/repository/usuario-repository.dart';
+import 'package:health_pets/repository/vacina-repository.dart';
 import 'package:health_pets/themes/color_theme.dart';
 import 'package:health_pets/widgets/widgets.dart';
 import 'package:health_pets/pages/pet.page.dart';
 import 'package:health_pets/repository/animal-repository.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'vacina.page.dart';
 
 confirmarExclusaoAnimal(BuildContext context, id) {
   var animalController = AnimalController(AnimalRepositoryy());
@@ -62,6 +66,35 @@ confirmarExclusaoUsuario(BuildContext context) {
                   exibirMensagem(context, '$value');
                   deleteToken(context);
                   setarMaterialPageRoute(context, LoginPage());
+                });
+              },
+              child: const Text('Sim'),
+            ),
+          ],
+        );
+      });
+}
+
+confirmarExclusaoVacina(BuildContext context, id) {
+  var vacinaController = VacinaController(VacinaRepository());
+  return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Alerta"),
+          content: Text("Deseja excluir este vacina?"),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context, rootNavigator: true).pop();
+              },
+              child: const Text('Não'),
+            ),
+            TextButton(
+              onPressed: () {
+                vacinaController.deleteVacina(id).then((value) {
+                  exibirMensagem(context, '$value');
+                  setarMaterialPageRoute(context, VacinaPage(id));
                 });
               },
               child: const Text('Sim'),
