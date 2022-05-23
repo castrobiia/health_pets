3bc26ab9305007eaf3182bcb237df3fc1a4d4c4a
castrobiia <beatriz.castro@creditas.com>
2022-05-23 01:35:21 -0300

    alteranod future builder do campo animal no diário

--- lib/pages/cadastro-diario.page.dart
+++ lib/pages/cadastro-diario.page.dart
@@ -69,129 +69,128 @@ class _CadastroDiarioState extends State<CadastroDiario> {
                 right: 30,
                 top: 20,
               ),
-              child: FutureBuilder<dynamic>(
-                  future: AnimalRepository().getAnimais(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState != ConnectionState.done) {
-                      return Center(
-                          child: Container(child: CircularProgressIndicator()));
-                    }
-                    if (snapshot.hasError) {
-                      return Center(
-                        child: Container(
-                          child:
-                              Text(AppLocalizations.of(context)!.errorLoading),
-                        ),
-                      );
-                    }
-                    List<dynamic> listaAnimais =
-                        AnimalRepository().toListAnimal(snapshot.data);
-                    return Form(
-                      child: Column(
-                        children: [
-                          setarCampoForms(tituloController, "Título", _titulo,
-                              validator: (value) => validarCampo(value)),
-                          DropdownButtonFormField(
-                            hint: Text(AppLocalizations.of(context)!.animal),
-                            validator: (value) {
-                              if (value == null) {
-                                return "Selecione o animal";
-                              }
-                              return null;
-                            },
-                            onSaved: (input) => _animal = input!.toString(),
-                            style: TextStyle(fontSize: 17, color: Colors.black),
-                            items: listaAnimais.map((item) {
-                              return DropdownMenuItem(
-                                child: new Text(
-                                  item.nome,
-                                  style: TextStyle(fontSize: 17),
-                                ),
-                                value: item.id,
-                              );
-                            }).toList(),
-                            onChanged: (newValue) {
-                              setState(() {
-                                animalController.text = newValue.toString();
-                              });
-                            },
-                            value: animalId,
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          TextFormField(
-                            autofocus: false,
-                            validator: (value) {
-                              if (value!.isEmpty) {
-                                return "Selecione uma data";
-                              }
-                              return null;
-                            },
-                            onSaved: (input) => _data = input!,
-                            decoration: InputDecoration(
-                              labelText: "Data",
-                              labelStyle: TextStyle(
-                                fontWeight: FontWeight.w400,
-                                fontSize: 17,
+              child: Form(
+                child: Column(children: [
+                  setarCampoForms(tituloController, "Título", _titulo,
+                      validator: (value) => validarCampo(value)),
+                  FutureBuilder<dynamic>(
+                      future: AnimalRepository().getAnimais(),
+                      builder: (context, snapshot) {
+                        // if (snapshot.connectionState != ConnectionState.done) {
+                        //   return Center(
+                        //       child: Container(
+                        //           child: CircularProgressIndicator()));
+                        // }
+                        // if (snapshot.hasError) {
+                        //   return Center(
+                        //     child: Container(
+                        //       child: Text(
+                        //           AppLocalizations.of(context)!.errorLoading),
+                        //     ),
+                        //   );
+                        // }
+                        List<dynamic> listaAnimais =
+                            AnimalRepository().toListAnimal(snapshot.data);
+                        return DropdownButtonFormField(
+                          hint: Text(AppLocalizations.of(context)!.animal),
+                          validator: (value) {
+                            if (value == null) {
+                              return "Selecione o animal";
+                            }
+                            return null;
+                          },
+                          onSaved: (input) => _animal = input!.toString(),
+                          style: TextStyle(fontSize: 17, color: Colors.black),
+                          items: listaAnimais.map((item) {
+                            return DropdownMenuItem(
+                              child: new Text(
+                                item.nome,
+                                style: TextStyle(fontSize: 17),
                               ),
-                            ),
-                            controller: dataController,
-                            readOnly: true,
-                            onTap: () {
-                              setState(
-                                () {
-                                  _dataSelecionada(context);
-                                },
-                              );
-                            },
-                          ),
-                          setarCampoForms(humorController, "Humor", _humor,
-                              validator: (value) => validarCampo(value)),
-                          //peso mudar para receber apenas numeros
-                          TextFormField(
-                            autofocus: false,
-                            keyboardType: TextInputType.number,
-                            controller: pesoController,
-                            //validator: (value) => validarCampo(value),
-                            onSaved: (input) => _peso = input!,
-                            decoration: InputDecoration(
-                              labelText: "Peso",
-                              labelStyle: TextStyle(
-                                fontWeight: FontWeight.w400,
-                                fontSize: 17,
-                              ),
-                            ),
-                          ),
-                          TextFormField(
-                            decoration: InputDecoration(
-                              labelText: "Descrição",
-                              labelStyle: TextStyle(
-                                fontWeight: FontWeight.w400,
-                                fontSize: 17,
-                              ),
-                            ),
-                            keyboardType: TextInputType.multiline,
-                            maxLines: 8,
-                            maxLength: 1000,
-                          ),
-                          SizedBox(
-                            height: 30,
-                          ),
-                          Container(
-                            width: double.infinity,
-                            decoration: botaoRetangulo(),
-                            child: TextButton(
-                              onPressed: () {
-                                setarMaterialPageRoute(context, Calendario());
-                              },
-                              child: textBotao("Salvar"),
-                            ),
-                          ),
-                        ],
+                              value: item.id,
+                            );
+                          }).toList(),
+                          onChanged: (newValue) {
+                            setState(() {
+                              animalController.text = newValue.toString();
+                            });
+                          },
+                          value: animalId,
+                        );
+                      }),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    autofocus: false,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Selecione uma data";
+                      }
+                      return null;
+                    },
+                    onSaved: (input) => _data = input!,
+                    decoration: InputDecoration(
+                      labelText: "Data",
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 17,
                       ),
-                    );
-                  }),
+                    ),
+                    controller: dataController,
+                    readOnly: true,
+                    onTap: () {
+                      setState(
+                        () {
+                          _dataSelecionada(context);
+                        },
+                      );
+                    },
+                  ),
+                  setarCampoForms(humorController, "Humor", _humor,
+                      validator: (value) => validarCampo(value)),
+                  //peso mudar para receber apenas numeros
+                  TextFormField(
+                    autofocus: false,
+                    keyboardType: TextInputType.number,
+                    controller: pesoController,
+                    //validator: (value) => validarCampo(value),
+                    onSaved: (input) => _peso = input!,
+                    decoration: InputDecoration(
+                      labelText: "Peso",
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Descrição",
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 17,
+                      ),
+                    ),
+                    keyboardType: TextInputType.multiline,
+                    maxLines: 8,
+                    maxLength: 1000,
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    decoration: botaoRetangulo(),
+                    child: TextButton(
+                      onPressed: () {
+                        setarMaterialPageRoute(context, Calendario());
+                      },
+                      child: textBotao("Salvar"),
+                    ),
+                  ),
+                ]),
+              ),
             ),
           ),
         ),
