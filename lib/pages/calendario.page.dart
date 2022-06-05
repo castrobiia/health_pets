ead5218ad737b1b3ecfbe1e32e57b23ac32c208b
castrobiia <beatriz.castro@creditas.com>
2022-06-05 01:17:23 -0300

    add drawer no calendário

--- lib/pages/calendario.page.dart
+++ lib/pages/calendario.page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:health_pets/pages/alert-dialog.dart';
+import 'package:health_pets/pages/logout.page.dart';
 import 'package:health_pets/pages/menu-calendario.page.dart';
+import 'package:health_pets/pages/pet.page.dart';
+import 'package:health_pets/repository/usuario-repository.dart';
 import 'package:health_pets/themes/color_theme.dart';
+import 'package:health_pets/widgets/widgets.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:flutter_clean_calendar/flutter_clean_calendar.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -97,7 +102,6 @@ class _CalendarioState extends State<Calendario> {
         backgroundColor: Colors.white,
         title: Text(AppLocalizations.of(context)!.calendar),
         centerTitle: true,
-        automaticallyImplyLeading: false,
         elevation: 1,
         actions: [
           PopupMenuButton<MenuItemCalendario>(
@@ -107,6 +111,94 @@ class _CalendarioState extends State<Calendario> {
             ],
           ),
         ],
+      ),
+      drawer: Drawer(
+        child: FutureBuilder<dynamic>(
+          future: UsuarioRepository().getUsuario(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState != ConnectionState.done) {
+              return Center(
+                  child: Container(child: CircularProgressIndicator()));
+            }
+            if (snapshot.hasError) {
+              return Center(
+                child: Container(
+                  child: Text(AppLocalizations.of(context)!.errorLoading),
+                ),
+              );
+            }
+            final usuario = snapshot.data["user"];
+            final qntdAnimais = snapshot.data["animais"];
+            return Column(
+              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // UserAccountsDrawerHeader(
+                //   arrowColor: Colors.pink,
+                //   accountName: Text(usuario['nome'] ?? ''),
+                //   accountEmail: Text(usuario['email'] ?? ''),
+                //   margin: EdgeInsets.only(bottom: 20),
+                // ),
+                Container(
+                  padding: EdgeInsets.only(left: 20, bottom: 20),
+                  width: double.maxFinite,
+                  color: ColorTheme.salmao1,
+                  height: 150,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        usuario['nome'] ?? '',
+                        style: TextStyle(
+                            fontSize: 23, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 5),
+                      Text(usuario['email'] ?? ''),
+                      SizedBox(height: 5),
+                      Text('Animais: ${qntdAnimais.toString()}'),
+                    ],
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.person,
+                  ),
+                  title: const Text('Editar Perfil'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.delete),
+                  title: const Text('Excluir Conta'),
+                  onTap: () {
+                    confirmarExclusaoUsuario(context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.logout),
+                  title: const Text('Sair'),
+                  onTap: () {
+                    setarMaterialPageRoute(context, LogoutPage());
+                  },
+                ),
+                Divider(color: Colors.black87),
+                ListTile(
+                  leading: Icon(Icons.pets),
+                  title: const Text('Pets'),
+                  onTap: () {
+                    setarMaterialPageRoute(context, PetPage());
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.calendar_today),
+                  title: const Text('Calendário'),
+                  onTap: () {
+                    setarMaterialPageRoute(context, Calendario());
+                  },
+                ),
+              ],
+            );
+          },
+        ),
       ),
       body: Container(
         color: Colors.white,
