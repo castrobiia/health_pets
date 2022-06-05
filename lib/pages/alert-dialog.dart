ba5e1e1e686f274e45a5e39d2bd3d7043c061a67
castrobiia <beatriz.castro@creditas.com>
2022-06-05 00:31:00 -0300

    add exclusão do diário

--- lib/pages/alert-dialog.dart
+++ lib/pages/alert-dialog.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:health_pets/controllers/animal_controller.dart';
+import 'package:health_pets/controllers/diario_controller.dart';
 import 'package:health_pets/controllers/usuario_controller.dart';
 import 'package:health_pets/controllers/vacina_controller.dart';
+import 'package:health_pets/pages/diario-pet.page.dart';
 import 'package:health_pets/pages/login.page.dart';
 import 'package:health_pets/pages/logout.page.dart';
 import 'package:health_pets/pages/tabs.page.dart';
 import 'package:health_pets/pages/vacina.page.dart';
+import 'package:health_pets/repository/diario-repository.dart';
 import 'package:health_pets/repository/usuario-repository.dart';
 import 'package:health_pets/repository/vacina-repository.dart';
 import 'package:health_pets/widgets/widgets.dart';
@@ -91,6 +94,35 @@ confirmarExclusaoVacina(BuildContext context, id) {
                 vacinaController.deleteVacina(id).then((value) {
                   exibirMensagem(context, '$value');
                   setarMaterialPageRoute(context, VacinaPage(id));
+                });
+              },
+              child: Text(AppLocalizations.of(context)!.yes),
+            ),
+          ],
+        );
+      });
+}
+
+confirmarExclusaoDiario(BuildContext context, idDiario, idAnimal) {
+  var diarioController = DiarioController(DiarioRepository());
+  return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(AppLocalizations.of(context)!.alert),
+          content: Text(AppLocalizations.of(context)!.deleteDiary),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context, rootNavigator: true).pop();
+              },
+              child: Text(AppLocalizations.of(context)!.no),
+            ),
+            TextButton(
+              onPressed: () {
+                diarioController.deleteDiario(idDiario).then((value) {
+                  exibirMensagem(context, '$value');
+                  setarMaterialPageRoute(context, DiarioPet(idAnimal));
                 });
               },
               child: Text(AppLocalizations.of(context)!.yes),
